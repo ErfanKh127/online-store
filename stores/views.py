1a3b025137e44f3d6790571723cb8c2a9d91996d
ErfanKh127 <erfankhosrobeygi127@gmail.com>
2026-08-25 15:15:46 +0330

    feat: add store API with owner-based access

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Store
+from .serializers import StoreSerializer
+
+
+class StoreViewSet(ModelViewSet):
+    serializer_class = StoreSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Store.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
